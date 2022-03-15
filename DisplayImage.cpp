fa5faa1bb235287a0be8daa074b379949f63a3d7
Alexander Rogers <68214947+alexanderrogers241@users.noreply.github.com>
2022-03-15 14:28:18 -0500

    added some changes

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -4,14 +4,14 @@
 #include <array>
 
 
-std::array<unsigned int, 33> PSA_DATA{0,1,2,3,
+std::array<unsigned int, 32> PSA_DATA{0,1,2,3,
                                       4,5,6,7,
                                       8,9,10,11,
                                      12,13,14,15,
                                      16,17,18,19,
                                      20,21,22,23,
                                      24,25,26,27,
-                                     28,29,30,31,101};
+                                     28,29,30,31};
 
 std::array<std::array<int, 2>, 1> TOPL = { {200, 25 } };
 std::array<std::array<int, 2>, 1> BOTR = { {460, 470} };
@@ -60,8 +60,62 @@ void drawx(pt& point, cv::Mat& frame)
 
 }
 
+bool InsidePSA(std::array<std::array<int, 2>, 2>& box, std::array<std::array<int, 2>, 1>& point)
+{
+    // points start on the top right corner and traverse clockwise
+    pt p1 = { TOPL[0] };
+    pt p2 = { TOPL[0][0] + 4 * WIDTH_SPACE, TOPL[0][1] };
+    pt p3 = { BOTR[0] };
+    pt p4 = { BOTR[0][0] - 4 * WIDTH_SPACE, BOTR[0][1] };
+
+    // point is plugged into 4 line equations that make up rectangle
+    // if the line equation f(x,y) > 0 point is to the right of vector
+    // if f(x,y) < 0 point is  to the left of vector
+    // if f(x,y) == 0 point is on the line
+    // traversing clockwise
+    // d1 = p1------------------------------------------>p2
+    int d1 = (p1[0][1] - p2[0][1]) * point[0][0] + (p2[0][0] - p1[0][0]) * point[0][1] + p1[0][0] * p2[0][1] - p2[0][0] * p1[0][1];
+    // d2 = p2
+    //       |
+    //       V
+    //      p3
+    int d2 = (p2[0][1] - p3[0][1]) * point[0][0] + (p3[0][0] - p2[0][0]) * point[0][1] + p2[0][0] * p3[0][1] - p3[0][0] * p2[0][1];
+    // d3 = p4<------------------------------------------p3
+    int d3 = (p3[0][1] - p4[0][1]) * point[0][0] + (p4[0][0] - p3[0][0]) * point[0][1] + p3[0][0] * p4[0][1] - p4[0][0] * p3[0][1];
+    // d4 = p1
+    //       ^
+    //       |
+    //      p4
+    int d4 = (p4[0][1] - p1[0][1]) * point[0][0] + (p1[0][0] - p4[0][0]) * point[0][1] + p4[0][0] * p1[0][1] - p1[0][0] * p4[0][1];
+
+    if (d1 < 0)
+    {
+        // above line d1
+        return false;
+    }
+    if (d2 <= 0)
+    {
+        // right and including line d2
+        return false;
+    }
+    if (d3 <= 0)
+    {
+        // below including line d3
+        return false;
+    }
+    if (d4 < 0)
+    {
+        // left of line d2
+        return false;
+    }
+
+    // if passes previous tests point is inside box
+    return true;
+}
+
 bool InsideBox(std::array<std::array<int, 2>, 2> box, std::array<std::array<int,2>,1> point)
 {
+    if (InsidePSA(box, point) == false) return false;
     // points start on the top right corner and traverse clockwise
     pt p1 = {box[0]};
     pt p2 = {box[0][0] + WIDTH_SPACE, box[0][1]};
@@ -113,7 +167,11 @@ bool InsideBox(std::array<std::array<int, 2>, 2> box, std::array<std::array<int,
     return true;
 }
 
-int PointToPressBox(std::vector<two_pts>& PSA_boxes, pt& point)
+
+
+
+
+bool PointToPressBox(std::vector<two_pts>& PSA_boxes, pt& point, int& out_index)
 {
 
     for (int i = 0; i < PSA_boxes.size(); i++) {
@@ -121,12 +179,14 @@ int PointToPressBox(std::vector<two_pts>& PSA_boxes, pt& point)
         {
             // returns index 0 - 31 to represent which pressure sensor the point is over
             // to map to PSA layout # subtract 31
-            return i;
+            out_index = i;
+            return true;
         }
         
     }
-    // returns hardcoded 101 integer for any point not on the PSA
-    return 32;
+    // return 101 value which means point is on edge of PSA box for a respective pressure sensor. Some edges while inside the PSA box are technically by edge case
+    // outside of the smaller box for each pressure sensor
+    return false;
 }
 
 int main()
@@ -151,12 +211,12 @@ int main()
             break;
         }
         // cv.rectangle(imageFrame, (TOPL[0][0], TOPL[0][1]), (BOTR[0][0], BOTR[0][1]), (0, 0, 255), thickness = 2)
-       /* cv::rectangle(frame,
+        cv::rectangle(frame,
             cv::Point(TOPL[0][0], TOPL[0][1]),
             cv::Point(BOTR[0][0], BOTR[0][1]),
             cv::Scalar(0, 0, 255),
             2,
-            cv::LINE_8);*/
+            cv::LINE_8);
 
         //BOX_TOPL = [[TOPL[0][0], TOPL[0][1]]]
         //BOX_BOTR = [[int(TOPL[0][0] + WIDTH_SPACE), int(TOPL[0][1] + HEIGHT_SPACE)]]
@@ -212,10 +272,14 @@ int main()
             BOX_TOPL[0][0] = BOX_TOPL[0][0] + (4 * WIDTH_SPACE);
             BOX_BOTR[0][0] = BOX_BOTR[0][0] + (4 * WIDTH_SPACE);
         }
-
-        pt point = { 460,470 };
+        int PressSenseIndex{ 101 };
+        pt point = { 400,450 };
         drawx(point, frame);
-        std::cout << PSA_DATA[PointToPressBox(PSA_boxes, point)] << "\n";
+        if (PointToPressBox(PSA_boxes, point, PressSenseIndex))
+        {
+            std::cout << PSA_DATA[PressSenseIndex] << "\n";
+        } 
+
         // show live and wait for a key with timeout long enough to show images
         imshow("Live", frame);
         if (cv::waitKey(5) >= 0)
